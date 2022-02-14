2d917cc43c8bff7b68a7571d331e92aabdb9c80c
Chia-Yu Hu <cyhu.astro@gmail.com>
2022-02-14 01:22:35 +0100

    fix

--- MakeGlassBox.jl
+++ MakeGlassBox.jl
@@ -277,7 +277,3 @@ end
 
 end #module MakeGlassBox
 
-using .MakeGlassBox
-NDIM = 2
-par = Params{NDIM,Float64}(nH=1.0, T_mu=1e4, boxsize=0.1, Ngas=1000, flag_plot_part=true, ms=3)
-generate_ICfile_glass_box(par)
